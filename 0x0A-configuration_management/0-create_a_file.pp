4a781f0eb920de0b21c90ea14477a914e7fef441
Felipe Layos <jfelipelayos@gmail.com>
2020-08-04 09:38:25 -0500

    fix

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
 # Create file
 file { '/tmp/holberton':
-    mode    =>  '0744',
+    ensure  =>  'file',
     owner   =>  'www-data',
     group   =>  'www-data',
     content =>  'I love Puppet',
+    mode    =>  '0744',
 }
