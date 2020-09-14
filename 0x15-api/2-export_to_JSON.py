58c3e9540d09e7f759d0005dcc52f52b9887a545
Felipe Layos <jfelipelayos@gmail.com>
2020-09-14 18:10:48 -0500

    Fix documentation

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Python script to export data in the JSON format."""
+"""API Module"""
 import json
 import requests
 import sys
--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""API Module"""
+import json
+import requests
+
+
+if __name__ == "__main__":
+    users = requests.get("https://jsonplaceholder.typicode.com/users").json()
+    todos = requests.get("https://jsonplaceholder.typicode.com/todos").json()
+
+    to_json = {}
+    for user in users:
+        tasks = []
+        for key in todos:
+            if key["userId"] == user["id"]:
+                my_dict = {}
+                my_dict["task"] = key.get("title")
+                my_dict["completed"] = key.get("completed")
+                my_dict["username"] = user.get("username")
+                tasks.append(my_dict)
+        to_json[user["id"]] = tasks
+    with open("todo_all_employees.json", "w") as file:
+        json.dump(to_json, file)
