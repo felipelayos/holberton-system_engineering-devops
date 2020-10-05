d5a1cb8dae2316286987805767a1c3f5e842a912
Felipe Layos <jfelipelayos@gmail.com>
2020-10-05 10:37:47 -0500

    Sky is the limit, let's bring that limit higher

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,1 +1,5 @@
-#!/usr/bin/env bash
+# open files
+exec {'fix_name':
+  path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
+  command => "sed -i 's/15/1000/g' /etc/default/nginx && service nginx restart"
+}
