c4eb1e34968b9eca078fc602a7bb1df18a1d0edb
Felipe Layos <jfelipelayos@gmail.com>
2020-10-05 10:39:55 -0500

    User limit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,1 +1,5 @@
-#!/usr/bin/env bash
+# amount of open files
+exec {'fix_file':
+  path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
+  command => "sed -i 's/4/1000/g' /etc/security/limits.conf && sed -i 's/5/1000/g' /etc/security/limits.conf"
+}
